fdcd943abb6c98585bfae72451a598988c6fb909
Seth A. Johnson <sethajoh@umich.edu>
2019-10-17 12:43:10 -0400

    bootstrap, font-awesome

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { FaGulp } from 'react-icons/fa'
 
 const IndexPage = () => (
   <Layout>
@@ -10,6 +11,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <FaGulp />
   </Layout>
 )
 
