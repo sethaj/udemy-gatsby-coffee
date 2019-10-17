ca156178bde984e7d8ad43dc4426da8e6a904339
Seth A. Johnson <sethajoh@umich.edu>
2019-10-17 14:26:34 -0400

    navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import Navbar from './Global/Navbar'
 
 import "./bootstrap.min.css"
 import "./layout.css"
@@ -15,6 +16,7 @@ import "./layout.css"
 const Layout = ({ children }) => {
   return(
     <>
+      <Navbar />
       {children}
     </>
   )
